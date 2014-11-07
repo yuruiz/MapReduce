c54e74e7e5157e529a5c0ac150fc5323803dafc4
Zhou Yurui <accountregistor@gmail.com>
2014-11-07 14:02:38 -0500

    worker structure

---
+++ src/task/MapperThread.java
@@ -0,0 +1,14 @@
+package task;
+
+public class MapperThread extends Thread{
+
+    private MapTask task;
+
+    public MapperThread(MapTask task) {
+        this.task = task;
+    }
+
+    public void run() {
+
+    }
+}
---
+++ src/task/ReducerThread.java
@@ -0,0 +1,14 @@
+package task;
+
+public class ReducerThread extends Thread{
+
+    ReduceTask task;
+
+    public ReducerThread(ReduceTask task) {
+        this.task = task;
+    }
+
+    public void run() {
+
+    }
+}
